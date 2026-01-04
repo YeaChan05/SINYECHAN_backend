a7f314f8f1a6a1a5f66f0b9d1954797b069448fa
YeaChan05 <qkenrdl05@gmail.com>
2026-01-04 17:32:45 +0900

    feat(common): BaseEntity에 기본 id 필드 및 equals/hashCode 메서드 추가

--- common/repository-jpa/src/main/java/org/yechan/remittance/BaseEntity.java
+++ common/repository-jpa/src/main/java/org/yechan/remittance/BaseEntity.java
@@ -1,19 +1,33 @@
 package org.yechan.remittance;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import java.time.LocalDateTime;
+import java.util.Objects;
+import org.hibernate.proxy.HibernateProxy;
+import org.jspecify.annotations.Nullable;
+
 
 @MappedSuperclass
 abstract class BaseEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
   @Column(updatable = false)
   LocalDateTime createdAt;
 
   @Column
   LocalDateTime updatedAt;
+
+  protected BaseEntity() {
+  }
 
   @PrePersist
   public void prePersist() {
@@ -25,5 +39,37 @@ abstract class BaseEntity {
   @PreUpdate
   public void preUpdate() {
     updatedAt = LocalDateTime.now();
+  }
+
+  @Nullable
+  public Long getId() {
+    return id;
+  }
+
+  @Override
+  public final boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null) {
+      return false;
+    }
+    Class<?> oEffectiveClass = o instanceof HibernateProxy
+        ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass()
+        : o.getClass();
+    Class<?> thisEffectiveClass = this instanceof HibernateProxy
+        ? ((HibernateProxy) this).getHibernateLazyInitializer()
+        .getPersistentClass() : this.getClass();
+    if (thisEffectiveClass != oEffectiveClass) {
+      return false;
+    }
+    BaseEntity that = (BaseEntity) o;
+    return getId() != null && Objects.equals(getId(), that.getId());
+  }
+
+  @Override
+  public final int hashCode() {
+    return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer()
+        .getPersistentClass().hashCode() : getClass().hashCode();
   }
 }
