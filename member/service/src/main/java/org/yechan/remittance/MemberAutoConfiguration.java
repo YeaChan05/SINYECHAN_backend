63c781a9f5896f1178494b903396ad1096d4e132
YeaChan05 <qkenrdl05@gmail.com>
2026-01-05 23:53:02 +0900

    feat(member): JWT 토큰 생성 및 조회 기능 추가

--- member/service/src/main/java/org/yechan/remittance/MemberAutoConfiguration.java
+++ member/service/src/main/java/org/yechan/remittance/MemberAutoConfiguration.java
@@ -1,9 +1,11 @@
 package org.yechan.remittance;
 
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 @AutoConfiguration
+@EnableConfigurationProperties(AuthTokenProperties.class)
 class MemberAutoConfiguration {
 
   @Bean
@@ -11,5 +13,21 @@ class MemberAutoConfiguration {
       MemberRepository memberRepository,
       PasswordHashEncoder passwordHashEncoder) {
     return new MemberService(memberRepository, passwordHashEncoder);
+  }
+
+  @Bean
+  MemberQueryUseCase memberQueryUseCase(
+      MemberRepository memberRepository,
+      PasswordHashEncoder passwordHashEncoder,
+      TokenGenerator tokenGenerator) {
+    return new MemberQueryService(memberRepository, passwordHashEncoder, tokenGenerator);
+  }
+
+  @Bean
+  TokenGenerator tokenGenerator(AuthTokenProperties authTokenProperties) {
+    return new JwtTokenGenerator(
+        authTokenProperties.salt(),
+        authTokenProperties.accessExpiresIn(),
+        authTokenProperties.refreshExpiresIn());
   }
 }
