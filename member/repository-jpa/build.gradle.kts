6901661e9e84e44a7d1cbd6554b194f156b98e0d
YeaChan05 <qkenrdl05@gmail.com>
2026-01-05 19:22:12 +0900

    test(member): JPA 통합 테스트 의존성 추가

--- member/repository-jpa/build.gradle.kts
+++ member/repository-jpa/build.gradle.kts
@@ -1,8 +1,10 @@
 dependencies {
+    implementation(project(":common:repository-jpa"))
     implementation(project(":member:infrastructure"))
 
     integrationTestImplementation("org.testcontainers:junit-jupiter")
     integrationTestImplementation("org.testcontainers:mysql")
+    integrationTestImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
     integrationTestRuntimeOnly("com.mysql:mysql-connector-j") {
         exclude(group = "com.google.protobuf", module = "protobuf-java")
     }
