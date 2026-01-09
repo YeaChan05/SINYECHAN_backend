677e8288b53bba7451fccb7de2c7b52ebe1fdd80
YeaChan05 <qkenrdl05@gmail.com>
2026-01-09 17:36:31 +0900

    feat(transfer): TransferAutoConfiguration 추가 및 관련 빈 등록

---
+++ transfer/service/src/main/java/org/yechan/remittance/transfer/TransferAutoConfiguration.java
@@ -0,0 +1,52 @@
+package org.yechan.remittance.transfer;
+
+import java.time.Clock;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.yechan.remittance.account.AccountRepository;
+
+@AutoConfiguration
+class TransferAutoConfiguration {
+
+  @Bean
+  TransferIdempotencyHandler transferIdempotencyHandler(
+      IdempotencyKeyRepository idempotencyKeyRepository
+  ) {
+    return new TransferIdempotencyHandler(idempotencyKeyRepository);
+  }
+
+  @Bean
+  TransferProcessService transferProcessService(
+      AccountRepository accountRepository,
+      TransferRepository transferRepository,
+      OutboxEventRepository outboxEventRepository,
+      IdempotencyKeyRepository idempotencyKeyRepository
+  ) {
+    return new TransferProcessService(
+        accountRepository,
+        transferRepository,
+        outboxEventRepository,
+        idempotencyKeyRepository
+    );
+  }
+
+  @Bean
+  LedgerWriter ledgerWriter(LedgerRepository ledgerRepository) {
+    return new LedgerWriter(ledgerRepository);
+  }
+
+  @Bean
+  TransferCreateUseCase transferCreateUseCase(
+      TransferIdempotencyHandler idempotencyHandler,
+      TransferProcessService transferProcessService,
+      LedgerWriter ledgerWriter,
+      Clock clock
+  ) {
+    return new TransferService(
+        idempotencyHandler,
+        transferProcessService,
+        ledgerWriter,
+        clock
+    );
+  }
+}
