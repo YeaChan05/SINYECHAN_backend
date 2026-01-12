18f468b1990689c78aae985cf637d5a864360b61
YeaChan05 <qkenrdl05@gmail.com>
2026-01-12 23:41:33 +0900

    feat(transfer): IdempotencyKey 범위 파라미터 추가

--- transfer/api/src/main/java/org/yechan/remittance/transfer/IdempotencyKeyController.java
+++ transfer/api/src/main/java/org/yechan/remittance/transfer/IdempotencyKeyController.java
@@ -3,8 +3,10 @@ package org.yechan.remittance.transfer;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.yechan.remittance.LoginUserId;
+import org.yechan.remittance.transfer.IdempotencyKeyProps.IdempotencyScopeValue;
 import org.yechan.remittance.transfer.dto.IdempotencyKeyCreateResponse;
 
 @RestController
@@ -15,13 +17,23 @@ record IdempotencyKeyController(
 
   @Override
   @PostMapping
-  public ResponseEntity<IdempotencyKeyCreateResponse> create(@LoginUserId Long memberId) {
-    var created = idempotencyKeyCreateUseCase.create(new IdempotencyKeyCreateCommand(memberId));
+  public ResponseEntity<IdempotencyKeyCreateResponse> create(
+      @LoginUserId Long memberId,
+      @RequestParam(required = false) IdempotencyScopeValue scope
+  ) {
+    IdempotencyScopeValue resolvedScope = scope == null
+        ? IdempotencyScopeValue.TRANSFER
+        : scope;
+    var created = idempotencyKeyCreateUseCase.create(
+        new IdempotencyKeyCreateCommand(memberId, resolvedScope));
     return ResponseEntity.ok(
         new IdempotencyKeyCreateResponse(created.idempotencyKey(), created.expiresAt()));
   }
 
-  private record IdempotencyKeyCreateCommand(long memberId) implements IdempotencyKeyCreateProps {
+  private record IdempotencyKeyCreateCommand(
+      long memberId,
+      IdempotencyScopeValue scope
+  ) implements IdempotencyKeyCreateProps {
 
   }
 }
