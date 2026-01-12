988db23682a46138e3dfbafacb1087d9a6b88e51
YeaChan05 <qkenrdl05@gmail.com>
2026-01-12 15:46:38 +0900

    test(remittance): 통합 테스트에 테스트 컨테이너 상속 추가

--- aggregate/src/integrationTest/java/org/yechan/remittance/IntegrationTest.java
+++ aggregate/src/integrationTest/java/org/yechan/remittance/IntegrationTest.java
@@ -1,17 +1,18 @@
 package org.yechan.remittance;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.client.RestTestClient;
 
 @SpringBootTest(classes = AggregateApplication.class,
     webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class IntegrationTest {
+public class IntegrationTest extends TestContainerSetup {
 
   @Autowired
   RestTestClient restTestClient;
 
-  @org.junit.jupiter.api.Test
+  @Test
   void test() {
     restTestClient.get().uri("/actuator/health")
         .exchange()
