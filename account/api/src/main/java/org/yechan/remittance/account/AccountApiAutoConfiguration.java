7dc396e3694e4a6de61b58c98ebcea065886c4a5
YeaChan05 <qkenrdl05@gmail.com>
2026-01-12 15:42:09 +0900

    feat(account): 알림 전송 및 이벤트 처리 관련 빈 등록 추가

--- account/api/src/main/java/org/yechan/remittance/account/AccountApiAutoConfiguration.java
+++ account/api/src/main/java/org/yechan/remittance/account/AccountApiAutoConfiguration.java
@@ -1,10 +1,20 @@
 package org.yechan.remittance.account;
 
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 @AutoConfiguration
-@Import(AccountController.class)
+@Import({AccountController.class, NotificationApiController.class})
 public class AccountApiAutoConfiguration {
 
+  @Bean
+  NotificationSessionRegistry notificationSessionRegistry() {
+    return new NotificationSessionRegistry();
+  }
+
+  @Bean
+  NotificationPushPort notificationPushPort(NotificationSessionRegistry registry) {
+    return new NotificationPushAdapter(registry);
+  }
 }
--- account/repository-jpa/src/main/java/org/yechan/remittance/account/repository/AccountRepositoryAutoConfiguration.java
+++ account/repository-jpa/src/main/java/org/yechan/remittance/account/repository/AccountRepositoryAutoConfiguration.java
@@ -6,14 +6,23 @@ import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.yechan.remittance.account.AccountRepository;
+import org.yechan.remittance.account.ProcessedEventRepository;
 
 @AutoConfiguration(before = DataJpaRepositoriesAutoConfiguration.class)
-@EntityScan(basePackageClasses = AccountEntity.class)
-@EnableJpaRepositories(basePackageClasses = AccountJpaRepository.class)
+@EntityScan(basePackageClasses = {AccountEntity.class, ProcessedEventEntity.class})
+@EnableJpaRepositories(basePackageClasses = {
+    AccountJpaRepository.class,
+    ProcessedEventJpaRepository.class
+})
 public class AccountRepositoryAutoConfiguration {
 
   @Bean
   AccountRepository accountRepository(AccountJpaRepository repository) {
     return new AccountRepositoryImpl(repository);
+  }
+
+  @Bean
+  ProcessedEventRepository processedEventRepository(ProcessedEventJpaRepository repository) {
+    return new ProcessedEventRepositoryImpl(repository);
   }
 }
